0f18bb91ddb0a34bf59e86ea2ed330d4eb0b4190
Julien Jerphanion <git@jjerphan.xyz>
2025-12-16 09:07:29 +0100

    Distribute for `linux_aarch64` (#4)
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,7 @@ mkdir build
 cd build
 
 cmake -G "NMake Makefiles" ^
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,12 @@
 mkdir build
 cd build
 cmake \
+  -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
   -DBUILD_SHARED_LIBS=ON \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_BUILD_TYPE:STRING=Release \
   -DRC_ENABLE_GTEST=ON \
   ..
-  
+
 make
 make install
